0fdc222fbd2833040f1f4b8b7709758aa8e1b58e
René Valencia Vallejo <renevalencia@gmail.com>
2023-09-22 15:06:09 -0500

    Added Spanish version of the home and privacy policy pages

---
+++ src/i18n/ui.ts
@@ -0,0 +1,28 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.challenges": "Races & challenges",
+    "nav.sign-in": "Sign in",
+    "nav.sign-up": "Sign up",
+    "nav.privacy-policy": "Privacy policy",
+    "nav.terms-of-use": "Terms of use",
+    "nav.support": "Support",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.challenges": "Carreras y retos",
+    "nav.sign-in": "Iniciar sesión",
+    "nav.sign-up": "Crear cuenta",
+    "nav.privacy-policy": "Política de privacidad",
+    "nav.terms-of-use": "Términos de uso",
+    "nav.support": "Soporte",
+  },
+} as const;
