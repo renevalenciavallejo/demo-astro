5752ed05fa59ba25749714ab2d33df4ac07bafad
René Valencia Vallejo <renevalencia@gmail.com>
2023-10-03 12:28:27 -0500

    New October challenges were added

--- src/components/LocalDate.tsx
+++ src/components/LocalDate.tsx
@@ -1,15 +1,15 @@
 import { format } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 
-function LocalDate({ dateUtc }) {
+interface Props {
+  dateUtc: Date
+}
+
+const LocalDate = ({ dateUtc }: Props) => {
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const localDate = utcToZonedTime(dateUtc, userTimeZone);
-
-  return (
-    <>
-      <span>{format(localDate, " MMM d, yyyy HH:mm:ss (zzzz)")}</span>
-    </>
-  );
-}
+  
+  return format(localDate, " MMM d, yyyy HH:mm:ss (zzzz)");
+};
 
 export default LocalDate;
