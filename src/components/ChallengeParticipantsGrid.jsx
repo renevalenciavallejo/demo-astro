256488b7c68c61715bbf45f2fefd81af7052d831
René Valencia Vallejo <renevalencia@gmail.com>
2023-10-16 18:44:59 -0500

    Challenges mdx files were updated

--- src/components/ChallengeParticipantsGrid.jsx
+++ src/components/ChallengeParticipantsGrid.jsx
@@ -358,9 +358,9 @@ function ChallengeParticipantsGrid({ participants, activityType, lang }) {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M5 1 1 5l4 4"
                     />
                   </svg>
@@ -397,9 +397,9 @@ function ChallengeParticipantsGrid({ participants, activityType, lang }) {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m1 9 4-4-4-4"
                     />
                   </svg>
